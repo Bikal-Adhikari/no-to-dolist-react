bad9d22f3d03710553d42f94cff73a0acbf33e14
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-04-02 10:49:18 +1100

    switching task and deleting task

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,31 @@ function App() {
   const addNewTask = (taskObj) => {
     setEntryList([...entryList, taskObj]);
   };
+  const switchTask = (id, type) => {
+    const tempArg = entryList.map((item) => {
+      if (item.id === id) item.type = type;
+
+      return item;
+    });
+    setEntryList(tempArg);
+  };
+
+  const handOnDelete = (id) => {
+    if (window.confirm("Are you sure, you want to delete the item?")) {
+      setEntryList(entryList.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <div className="wrapper vh-100 pt-5">
       <div className="container">
         <Title />
         <Form addNewTask={addNewTask} />
-        <Table entryList={entryList} />
+        <Table
+          entryList={entryList}
+          switchTask={switchTask}
+          handOnDelete={handOnDelete}
+        />
         <div className="alert alert-success" role="alert">
           The total hours allocated = <span id="ttlHrs">0</span>hrs
         </div>
